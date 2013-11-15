3713b6cc986b30eaabf7577c5b450d7b419438d0
steenreem <steen.reem@gmail.com>
2013-11-15 17:20:48 +0100

    improved toString methods of Types

--- src/typeChecker/Type.scala
+++ src/typeChecker/Type.scala
@@ -2,5 +2,14 @@ package typeChecker
 
 abstract class Type
 object IntType extends Type
+{
+  override def toString() = "Int"
+}
 case class LambdaType(input: Type, output: Type) extends Type
+{
+  override def toString() = s"$input => $output"
+}
 case class VariableType(name: String) extends Type
+{
+  override def toString() = "$" + name
+}
